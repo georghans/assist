9f428f60a49d107444e2f1c86affcd14ec9422c5
Georg Hans <georg.hans@rio.cloud>
2025-12-16 20:36:53 +0100

    add telegram bot

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import {Telegraf} from "npm:telegraf@4.16.3";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+const telegramBot = new Telegraf(Deno.env.get("TELEGRAM_BOT_TOKEN")!);
+
+telegramBot.start((ctx) => console.log(ctx));
+
+telegramBot.on("message", async (ctx) => {
+  console.log("message", ctx);
+  ctx.telegram.sendChatAction(ctx.message.chat.id, "typing");
+
+  const senderName = ctx.message?.from?.first_name
+    ? `${ctx.message?.from?.first_name} ${ctx.message?.from?.last_name}`
+    : ctx.message?.from?.username || null;
+
+  await ctx.reply(`Hello ${senderName || "there"}! You said: "${ctx.message}"`);
 });
